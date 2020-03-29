18f145fc0e0c8a18b8636d0bcc1ead568d204cad
alienindustry <richardbs@hotmail.co.uk>
2020-03-29 20:36:08 +0100

    package updates

--- server.js
+++ server.js
@@ -1,45 +1,68 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var handlebars = require('express-handlebars');
+var handlebars = require("express-handlebars");
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs'
-  }))
+var Contact = require(,./models/Contact');
+
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs"
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.render('index', { layout: 'main' });
+  Contact.find({}).lean()
+  .exec((err, contacts) =>{
+    if(contacts. length){
+      res.render('index', { layout: 'main', contacts: contacts, contactsExist: true}); 
+    } else {
+        res.render('index', { layout: 'main', contacts: contacts, contractsExist: false }); 
+    } 
+    })
+});
+
+app.post('/addcontact', (req, res) => {
+  const { name, email, number } = req.body;
+  var contact = new Contact({
+    name,
+    email,
+    number
+  });
+  contact.save();
+  res.redirect('/');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { layout: 'main' });
 });
 //Mongo is used to connect to our data base 27017 is our port no
-mongoose.connect('mongodb://localhost:27017/handlebars',{
-     //These are used so we do not get depreciation messages
+mongoose
+  .connect('mongodb://localhost:27017/handlebars', {
+    //These are used so we do not get depreciation messages
     useUnifiedTopology: true,
     useNewUrlParser: true
-})
-.then(() => {
-  //Console logging to say we are conncted to our DB
-   console.log('connected to the DB');
-})
+  })
+  .then(() => {
+    //Console logging to say we are conncted to our DB
+    console.log("connected to the DB");
+  })
 
-// .catch to catch and stop the process if there has been an error
-.catch((err) =>{
-   console.log('Not connected to the DB with err : ' + err);
-
-
-})
-
-
+  // .catch to catch and stop the process if there has been an error
+  .catch(err => {
+    console.log("Not connected to the DB with err : " + err);
+  });
 
 //listening for a request on port 3000
 app.listen(3000, () => {
-    console.log('server listening on port 3000');
+  console.log('server listening on port 3000');
 });
